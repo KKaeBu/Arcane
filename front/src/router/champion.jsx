5b620e4c364501f3988d269addeee55f3d0eedf9
unwind <ysh038@naver.com>
2022-06-25 17:20:03 +0900

    make button

--- front/src/router/champion.jsx
+++ front/src/router/champion.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 
 function Champion() {
+    const [style, setStyle] = useState({ display: "none" });
+
     const champion_url =
         "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/ko_KR/champion.json";
 
@@ -17,19 +19,36 @@ function Champion() {
         getChamp();
     }, []);
 
-    const handleClick = () => {
+    const handleAatroxClick = () => {
         if (display === champions) {
             setDisplay(champions.Aatrox);
+            setStyle({ display: "block" });
         } else if (display !== champions) {
             setDisplay(champions);
+            setStyle({ display: "none" });
         }
     };
     return (
         <div className="mainDisplay">
             <h1>Riot API Practice</h1>
             <h3>champion data</h3>
-            <button onClick={handleClick}>Click me</button>
-            <div>{JSON.stringify(display, null, "\t")}</div>
+            <button onClick={handleAatroxClick}>아트록스</button>
+            <div style={style}>
+                <span> {JSON.stringify(display, null, "\t")}</span>
+                <table>
+                    <th>Column 1</th>
+                    <th>Column 2</th>
+                    <th>Column 3</th>
+                    <tr>
+                        <td>Row 1, Column 1</td>
+                        <td>Row 1, Column 2</td>
+                        <td>Row 1, Column 3</td>
+                    </tr>
+                    <tr>
+                        <td>Row 2, Column 1</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
