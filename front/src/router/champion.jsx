e4b5ebb7cd115d055349977eeda73b02e16e8b33
unwind <ysh038@naver.com>
2022-06-27 18:52:50 +0900

    Champion List Button

--- front/src/router/champion.jsx
+++ front/src/router/champion.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react";
 
 function Champion() {
-    const [style, setStyle] = useState({ display: "none" });
+    const mainDiv = document.getElementsByClassName("mainDisplay");
+    const btnDiv = document.getElementsByClassName("button_div");
+    const listBtn = document.getElementById("champList");
 
     const champion_url =
         "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/ko_KR/champion.json";
 
-    const [champions, setChampion] = useState([]);
-    const [display, setDisplay] = useState([]);
+    const [style, setStyle] = useState({ display: "none" });
+    const [champions, setChampion] = useState({});
+    const [display, setDisplay] = useState({});
 
     const getChamp = async () => {
         const json = await (await fetch(champion_url)).json();
+
         setChampion(json.data, null, "\t");
         setDisplay(json.data, null, "\t");
     };
@@ -18,6 +22,33 @@ function Champion() {
     useEffect(() => {
         getChamp();
     }, []);
+
+    const showChampBtn = () => {
+        btnDiv[0].style.display = "block";
+        if (btnDiv[0].childNodes.length === 0) {
+            for (const key in champions) {
+                if (champions.hasOwnProperty(key)) {
+                    const btn = document.createElement("button");
+                    const btn_text = document.createTextNode(
+                        JSON.stringify(champions[key].name, null, "\t")
+                    );
+                    btn.appendChild(btn_text);
+                    btnDiv[0].appendChild(btn);
+                }
+            }
+        }
+
+        const backBtn = document.createElement("button");
+        const backBtn_text = document.createTextNode("뒤로가기");
+        backBtn.onclick = function () {
+            btnDiv[0].style.display = "none";
+            this.remove();
+            listBtn.style.display = "inline";
+        };
+        backBtn.appendChild(backBtn_text);
+        mainDiv[0].appendChild(backBtn);
+        listBtn.style.display = "none";
+    };
 
     const handleAatroxClick = () => {
         if (display === champions) {
@@ -28,26 +59,28 @@ function Champion() {
             setStyle({ display: "none" });
         }
     };
+
     return (
         <div className="mainDisplay">
             <h1>Riot API Practice</h1>
             <h3>champion data</h3>
+            <div className="button_div"></div>
             <button onClick={handleAatroxClick}>아트록스</button>
+            <button id="champList" onClick={showChampBtn}>
+                챔피언 목록
+            </button>
             <div style={style}>
                 <span> {JSON.stringify(display, null, "\t")}</span>
-                <table>
-                    <th>Column 1</th>
-                    <th>Column 2</th>
-                    <th>Column 3</th>
+                {/* <table>
+                    <th>name</th>
+                    <th>title</th>
+                    <th>difficulty</th>
                     <tr>
-                        <td>Row 1, Column 1</td>
-                        <td>Row 1, Column 2</td>
-                        <td>Row 1, Column 3</td>
+                        <td>{JSON.stringify(display.name, null, "\t")}</td>
+                        <td>{JSON.stringify(display.title, null, "\t")}</td>
+                        <td>{JSON.stringify(display.info, null, "\t")}</td>
                     </tr>
-                    <tr>
-                        <td>Row 2, Column 1</td>
-                    </tr>
-                </table>
+                </table> */}
             </div>
         </div>
     );
