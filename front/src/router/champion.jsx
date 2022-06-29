0d45d5bbf887221e409dd158d35a4fa2256bc9ab
unwind <ysh038@naver.com>
2022-06-29 15:29:19 +0900

    put img

--- front/src/router/champion.jsx
+++ front/src/router/champion.jsx
@@ -5,13 +5,18 @@ function Champion() {
     const btnDiv = document.getElementsByClassName("button_div");
     const listBtn = document.getElementById("champList");
     const info = document.getElementById("champInfo");
+    // mainDiv > 이 div 안에 champion컴포넌트 내용이 들어감
+    // btnDiv > 버튼을 집어넣을 div
+    // listBtn > 챔피언 목록을 불러오는 버튼
+    // info > 챔피언 정보 표시할 span태그
 
     const champion_url =
         "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/ko_KR/champion.json";
-
+    // LOL 챔피언 정보 API
     const [style, setStyle] = useState({ display: "none" });
     const [champions, setChampion] = useState({});
     const [display, setDisplay] = useState({});
+    const [champion_img, setImg] = useState({});
 
     const getChamp = async () => {
         const json = await (await fetch(champion_url)).json();
@@ -25,18 +30,35 @@ function Champion() {
     }, []);
 
     const showChampBtn = () => {
-        btnDiv[0].style.display = "block";
+        const champ_img = document.createElement("img");
+
+        btnDiv[0].style.display = "block"; // btn div보이게 설정
         if (btnDiv[0].childNodes.length === 0) {
+            // btn div가 비었다면 실행
             for (const key in champions) {
+                // 챔피언 목록 object의 key값들
                 if (champions.hasOwnProperty(key)) {
+                    champ_img.src =
+                        "https://ddragon.leagueoflegends.com/cdn/10.6.1/img/champion/" +
+                        champions[key].id +
+                        ".png";
                     const btn = document.createElement("button");
                     const btn_text = document.createTextNode(
-                        JSON.stringify(champions[key].name, null, "\t")
+                        JSON.stringify(champions[key].name, null, "\t").replace(
+                            /['"]+/g,
+                            ""
+                        )
                     );
+                    //btn.appendChild(champ_img);
                     btn.onclick = function () {
                         if (display === champions) {
                             info.style.display = "block";
                             setDisplay(champions[key]);
+                            setImg(
+                                "https://ddragon.leagueoflegends.com/cdn/10.6.1/img/champion/" +
+                                    champions[key].id +
+                                    ".png"
+                            );
                             setStyle({ display: "block" });
                         } else if (display !== champions) {
                             setDisplay(champions);
@@ -66,12 +88,14 @@ function Champion() {
         <div className="mainDisplay">
             <h1>Riot API Practice</h1>
             <h3>champion data</h3>
+
             <div className="button_div"></div>
             <button id="champList" onClick={showChampBtn}>
                 챔피언 목록
             </button>
             <div>
                 <span id="champInfo" style={style}>
+                    <img src={champion_img} alt="" />
                     {JSON.stringify(display, null, "\t")}
                 </span>
                 {/* <table>
