11ba089770661949c474c5f31a90a8e5f6643548
GgeaBu <h2sorginal@gmail.com>
2022-07-05 00:30:24 +0900

    서버 구축 및 파일 정리
    서버를 새롭게 구축하고 프론트랑 서버부분 폴더 및 파일 상세 분류(틀잡기)

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+import MainDisplay from "./page/main";
 
 function App() {
-    const Riot_API_Key = "RGAPI-b02089a7-38b1-4cd3-8bb2-6ad7f37aefe9";
-    const sohwansa = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
-    const userName = "승수몬";
-    const link = sohwansa + userName + '?api_key=' + Riot_API_Key;
-    const newLink = 'http://ddragon.leagueoflegends.com/cdn/12.11.1/data/ko_KR/champion.json';
+    const [lodding, setLodding] = useState(false);
+    const callMain = async () => {
+        console.log('엑시오스 작동');
+        axios.get("/main") // mainDisplay
+            .then((res) => {
+                console.log(res.data)
+                setLodding(true);
+            })
+            .catch((error) => console.log("error 발생!!!!"))
+    };
 
-
-
-    const [me, setMe] = useState([]);
-
-    const getUser = async () => {
-        const json = await (
-            await fetch(newLink)
-        ).json();
-        console.log(json);
-        setMe(json.data.Aatrox);
-    }
-
-    useEffect(() => {
-        getUser();
-    }, [])
-
+    useEffect(()=>{
+        callMain();
+    }, []);
+    
     return (
-        <div className="mainDisplay">
-            <h1>Riot API Practice</h1>
-            <div>{JSON.stringify(me)}</div>
-        </div>
+      <div className="App">
+        {lodding ? (
+          <MainDisplay />
+        ) : (
+          <h1>Front axios connecting...</h1>
+        )}
+      </div>
     );
 }
 
---
+++ server/router/champion.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'express-async-errors';
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.send({ hi: "hello i'm champion router" });
+})
+
+export default router;
