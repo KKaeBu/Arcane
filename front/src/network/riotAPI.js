9bee5b7572ce284e17491bd000b506c9563e9ec6
GgeaBu <h2sorginal@gmail.com>
2022-11-01 17:08:30 +0900

    Update riotAPI.js

--- front/src/network/riotAPI.js
+++ front/src/network/riotAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Riot_API {
     // API_Key는 만료될때마다 바꿔 적어줘야함 (발급 후 24시간 후 만료)
     // Version 업데이트마다 변경해줘야함
-    #Riot_API_Key = "RGAPI-40469ef6-a9ad-4c49-bf90-d2db77f085a8";
+    #Riot_API_Key = "RGAPI-31c5e046-7376-424c-8513-470916dc9990";
     #Language = "ko_KR";
     #Version = "12.19.1";
     #headers = {
