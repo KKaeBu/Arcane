9b4604b198b732265d972e4dfe36318d649587b0
GgeaBu <h2sorginal@gmail.com>
2022-11-01 20:19:08 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -58,8 +58,8 @@ connectDB() //
         // initSocket(httpServer);
 
         var options = {
-            key: fs.readFileSync('./Arcane_key.pem'),
-            cert: fs.readFileSync('./Arcane_key.pem')
+            key: fs.readFileSync('./arcane.pem'),
+            cert: fs.readFileSync('./arcane.pem')
         };
         const httpsServer = https.createServer(options, app).listen(config.host.port, () => {
             console.log("listening on *: " + config.host.port);
