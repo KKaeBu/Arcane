1f2b79540bd6616ca575ce997cad7ee9df547f05
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2023-07-16 00:39:55 +0900

    라이엇 key 업데이트

--- front/src/network/riotAPI.js
+++ front/src/network/riotAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Riot_API {
     // API_Key는 만료될때마다 바꿔 적어줘야함 (발급 후 24시간 후 만료)
     // Version 업데이트마다 변경해줘야함
-    #Riot_API_Key = "RGAPI-cc04855b-6d0a-42df-8203-b9a07680c34a";
+    #Riot_API_Key = "RGAPI-646b6917-eefc-413a-bf1a-3b7469c04423";
     #Language = "ko_KR";
     #Version = "13.13.1";
     #headers = {
